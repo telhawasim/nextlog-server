8cc44efa13f7d43ca93d2342fb0ed45c04bb3906
Telha Wasim <telhawasim@gmail.com>
2025-04-01 14:45:37 +0500

    add response model in get all employee api

--- modules/employees/employee_router.py
+++ modules/employees/employee_router.py
@@ -5,7 +5,7 @@ router = APIRouter(tags=["Employee"], prefix="/employee")
 
 
 # In order to get all the employees
-@router.get("/all")
+@router.get("/all", response_model=GetAllEmployees)
 async def get_all_employees(
     page: int = Query(1, gt=0),  # Default 1, must be >= 1
     limit: int = Query(10, gt=0, le=100),  # Default 10, max 100
